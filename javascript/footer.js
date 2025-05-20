3c1dd78a0ae0e7329f34e970e9b594018acc76a0
Pietro <nencinipietro2006@gmail.com>
2025-05-20 08:44:42 +0200

    added footer

--- javascript/footer.js
+++ javascript/footer.js
@@ -5,7 +5,7 @@ if(space[0].classList.contains("has_footer")) {
 
 function createFooter() {
     let footer = document.createElement("footer");
-    footer.className = "bg-warning text-white py-4 mt-5";
+    footer.className = "bg-warning py-4 mt-5";
     
     let container = document.createElement("div");
     container.className = "container text-center text-md-start";
@@ -43,4 +43,3 @@ function createFooterColumn(title, text, icon) {
 
     return col;
 }
-
